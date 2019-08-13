c2c8104dfbe3f7b597789cc422c7819f3413e923
Tibor Schneider <tiborschneider@bluewin.ch>
2019-08-13 17:44:08 +0200

    Updated plot.py, added measurements

--- plots/plot.py
+++ plots/plot.py
@@ -1,15 +1,14 @@
 """
 plot hdlib data
 """
+
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
-CPU = np.genfromtxt("cpu_data.csv", delimiter=",")
-GPU = np.genfromtxt("gpu_data.csv", delimiter=",")
-GPU_BATCH = np.genfromtxt("gpu_batch_data_16.csv", delimiter=",")
-
-plt.scatter(CPU[:, 0], CPU[:, 1])
-plt.scatter(GPU[:, 0], GPU[:, 1])
-plt.scatter(GPU_BATCH[:, 0], GPU_BATCH[:, 1])
+for arg in sys.argv[1:]:
+    print(f'plotting {arg}')
+    data = np.genfromtxt(arg, delimiter=",")
+    plt.scatter(data[:, 0], data[:, 1])
 
 plt.show()
